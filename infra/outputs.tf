33c462b859ab235b4deea694799283acb98eb770
CHEBBI Talel <tchebbi.ext@MacBook-Pro.lan>
2026-05-24 00:27:31 +0200

    Remove duplicate output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -43,10 +43,3 @@ output "aws_bucket_name" {
   value = module.aws_static_site.bucket_name
 }
 
-output "aws_cloudfront_distribution_id" {
-  value = module.aws_static_site.cloudfront_distribution_id
-}
-
-output "azure_storage_account_name" {
-  value = module.azure_static_site.storage_account_name
-}

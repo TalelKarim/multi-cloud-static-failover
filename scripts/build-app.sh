6fd486390a4664a94916b43cecac7ee32b8207ce
CHEBBI Talel <tchebbi.ext@MacBook-Pro.lan>
2026-05-26 22:57:49 +0200

    Redeploy APP

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -11,6 +11,7 @@ cp app/index.html dist/
 cp app/script.js dist/
 cp app/styles.css dist/
 
+
 sed "s/__CLOUD_PROVIDER__/${CLOUD_PROVIDER}/g" \
   app/runtime-config.template.json \
   > dist/runtime-config.json
